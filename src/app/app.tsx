e4c357f348daa7517532ca808ee8424b85812f49
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-08-25 13:02:10 +0300

    Add files via upload

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter } from 'react-router-dom'
 import Router from "@app/router/router"
 import "@styles/default.scss"
 import MobileLimiter from '@/components/mobile/mobile-limiter'
-import '@telegram-apps/telegram-ui/dist/styles.css';
 
 createRoot(document.getElementById('root')!).render(
 	<MobileLimiter>
