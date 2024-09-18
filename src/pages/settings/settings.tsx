e4c0db147d6943145f3d065a3c19f3f84de56e02
Arthur Emanuilov <111119044+PlayGanster@users.noreply.github.com>
2024-09-18 15:54:05 +0300

    Add files via upload

--- src/pages/settings/settings.tsx
+++ src/pages/settings/settings.tsx
@@ -1,14 +1,19 @@
 import SettingsBody from '@/components/settings/settings-body'
 import SettingsHeader from '@/components/settings/settings-header'
 import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const tg = window.Telegram.WebApp
 
 const SettingsPage = () => {
 
+	const navigate = useNavigate();
+
 	useEffect(() => {
 		tg.BackButton.show()
 	}, [])
+
+	tg.BackButton.onClick(() => navigate("/"))
 
 	return (
 		<>
